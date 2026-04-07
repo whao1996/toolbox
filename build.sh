b382393c36b16ac09961ad85e1c33342e8ed1162
wanghao <wanghao16142@gmail.com>
2026-04-07 11:34:12 +0800

    [IMP] 更新打包脚本

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
+mkdir toolbox/src/main/resources/static
 cd toolbox-ui
 npm run build
 cp -r dist/* ../toolbox/src/main/resources/static
--- toolbox/src/main/java/com/example/toolbox/Application.java
+++ toolbox/src/main/java/com/example/toolbox/Application.java
@@ -2,11 +2,28 @@ package com.example.toolbox;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public ApplicationListener<ApplicationReadyEvent> applicationReadyEventListener(Environment environment) {
+        return event -> {
+            String port = environment.getProperty("server.port", "8080");
+            String contextPath = environment.getProperty("server.servlet.context-path", "");
+            String accessUrl = "http://localhost:" + port + contextPath + "/index.html";
+            System.out.println("\n🎉========================================🎉");
+            System.out.println("✅Toolbox is ready!");
+            System.out.println("🚀 Access Toolbox: " + accessUrl);
+            System.out.println("🎉========================================🎉\n");
+        };
     }
 }
